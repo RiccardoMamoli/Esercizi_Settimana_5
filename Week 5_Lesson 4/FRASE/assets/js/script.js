530ea79c5527a60f5bd20644c66cc9e78db18461
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-07-11 15:56:07 +0200

    compiling exercise

---
+++ Week 5_Lesson 4/FRASE/assets/js/script.js
@@ -0,0 +1,31 @@
+// const spinninword = document.getElementById('word')
+// const allWords = document.querySelectorAll('.parole i');
+// const wordsArray = [];
+
+
+// function createArray() {
+//     for (i = 0; i < allWords.length; i++) {
+//         const stringWord = allWords[i].innerHTML;
+//         wordsArray.push(stringWord);
+//     } return wordsArray;
+// }
+
+// console.log(wordsArray);
+
+// function showWord() {
+//     for (i = 0; i < wordsArray.length; i++) {
+//         spinninword.innerText= wordsArray[i];
+//     };
+// }
+
+// window.onload = function(){
+//     init();
+// };
+
+// function init() {
+//     createArray();
+//     showWord();
+// }
+
+
+
---
+++ Week 5_Lesson 4/POST_IT/assets/js/script.js
@@ -0,0 +1,1 @@
+const postIt = document.createElement('div')
